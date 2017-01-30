7e983e73c0fa34b5d56a8c387c9c70c7504aebdd
claudiobaronee <claudiobaronee@gmail.com>
2017-01-30 15:50:13 -0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server')
-
-  socket.emit('createMessage', {
-    from: 'claudio',
-    text: 'Hey. This is Claudio.'
-  });
 });
 
 socket.on('disconnect', function () {

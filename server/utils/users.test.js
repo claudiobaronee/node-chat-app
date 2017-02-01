4b2c956a69c36ba085a38d19d54614ce651c541b
claudiobaronee <claudiobaronee@gmail.com>
2017-02-01 16:20:00 -0200

    Add Users class and updateUserList event

---
+++ server/utils/users.test.js
@@ -0,0 +1,77 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Claudio',
+      room: 'Node Course'
+    },{
+      id: '2',
+      name: 'Barone',
+      room: 'Node Course2'
+    },{
+      id: '3',
+      name: 'Larissa',
+      room: 'Node Course'
+    }]
+  });
+
+  it('should add new user', () => {
+    var users = new Users();
+    var user = {
+      id: '123',
+      name: 'Andrew',
+      room: 'The Office Fans'
+    };
+    var resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+
+  it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove a user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
+
+    expect(user).toNotExist();
+    expect(users.users.length).toBe(3);
+  });
+
+  it('should find a user', () => {
+    var userId = '2';
+    var user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find a user', () => {
+    var userId = '99';
+    var user = users.getUser(userId);
+
+    expect(user).toNotExist();
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node Course');
+    expect(userList).toEqual(['Claudio', 'Larissa']);
+  });
+
+  it('should return names for node course2', () => {
+    var userList = users.getUserList('Node Course2');
+    expect(userList).toEqual(['Barone']);
+  });
+
+});
---
+++ server/utils/validation.test.js
@@ -0,0 +1,19 @@
+var expect = require('expect');
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('Should reject non-string values', () => {
+    var res = isRealString(98);
+    expect(res).toBe(false);
+  });
+
+  it('Should reject string with only spaces', () => {
+    var res = isRealString('    ');
+    expect(res).toBe(false);
+  });
+
+  it('Should allow string with non-space characters', () => {
+    var res = isRealString('   BARONE  ');
+    expect(res).toBe(true);
+  });
+});
